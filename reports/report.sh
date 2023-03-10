f4452ef35f1e126a99eff28eb12f9c90303fba75
Simon Norris <snorris@hillcrestgeo.ca>
2023-03-10 15:53:55 -0800

    do not call removed files

--- reports/report.sh
+++ reports/report.sh
@@ -4,8 +4,6 @@ set -euxo pipefail
 mkdir -p reports
 
 psql2csv $DATABASE_URL < sql/watershed_summary.sql > reports/watershed_summary.csv
-psql2csv $DATABASE_URL < sql/barrier_extent.sql > reports/barrier_extent.csv
-psql2csv $DATABASE_URL < sql/barrier_severity.sql > reports/barrier_severity.csv
 
 # just one value for each of these
 psql2csv $DATABASE_URL < sql/dci_elkr_above_elko.sql > reports/dci_elkr_above_elko.csv
